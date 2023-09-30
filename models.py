0641fbc034dfbd918bd20e991c80dcdf7c76dcd4
Herbeton <herbeton7@gmail.com>
2023-09-30 15:16:57 -0300

    Add model stocks-eua ao db.

--- models.py
+++ models.py
@@ -15,4 +15,15 @@ class Stock(Base):
     ma50 = Column(Numeric(10, 2))
     ma200 = Column(Numeric(10, 2))
 
+    __tablename__ = "stocks-eua"
+
+    id = Column(Integer, primary_key=True, index=True)
+    symbol = Column(String, unique=True, index=True)
+    marketCap = Column(Numeric(20, 2))
+    Country = Column(String, unique=True, index=True)
+    ipoYear = Column(Numeric(10, 2))
+    volume = Column(Numeric(20, 2))
+    sector = Column(String, unique=True, index=True)
+    Industry = Column(String, unique=True, index=True)
+
     
